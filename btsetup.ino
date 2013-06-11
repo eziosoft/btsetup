17cc436fee0fb377dd46b6602f5ef8cb06b4c10f
eziosoft <eziosoft@gmail.com>
2013-06-11 20:27:55 +0200

    3

--- btsetup.ino
+++ btsetup.ino
@@ -50,9 +50,9 @@ void BTSet()
 }
 
 void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);  
 }
 
